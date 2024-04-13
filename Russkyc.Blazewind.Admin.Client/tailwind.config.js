0db3144bbb15b5328c51fb0f455718bd2fcee2c9
Russell Camo <jrcc1023@gmail.com>
2024-04-13 22:52:59 +0800

    :wrench: Change tailwind implementation to tailwind utility, remove tailwind script

---
+++ Russkyc.Blazewind.Admin.Client/tailwind.config.js
@@ -0,0 +1,5 @@
+        /** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["*.razor", "./Pages/**/*.{html,js,cshtml,razor}", "./Shared/**/*.{html,js,cshtml,razor}"],
+    plugins: [],
+}
