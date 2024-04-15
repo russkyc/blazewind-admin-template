26ee63ca4d05c74bdcbe200b06946a7b6222d91d
Russell Camo <jrcc1023@gmail.com>
2024-04-15 22:05:15 +0800

    :lipstick: Update navigation style to use new BlazeNavLink, added component folder to tailwind config, remove in-script tailwind config

--- Russkyc.Blazewind.Admin.Client/tailwind.config.js
+++ Russkyc.Blazewind.Admin.Client/tailwind.config.js
@@ -1,5 +1,5 @@
         /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["*.razor", "./Pages/**/*.{html,js,cshtml,razor}", "./Shared/**/*.{html,js,cshtml,razor}"],
+    content: ["./Components/**/*.{html,js,cshtml,razor}", "./Pages/**/*.{html,js,cshtml,razor}", "./Shared/**/*.{html,js,cshtml,razor}"],
     plugins: [],
 }
